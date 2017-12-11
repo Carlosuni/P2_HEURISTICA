a448815fed4fdd7df5bf0d2cce1433c1698de90f
Carlos <carlos@carlos>
2017-12-11 03:52:53 +0100

    Creación de proyecto para Parte 2

---
+++ parte2_busq_heur/src/parte2_busq_heur/AstarParking.java
@@ -0,0 +1,16 @@
+/**
+ * @author Carlos Dumont & Sergio Casado
+ */
+package parte2_busq_heur;
+
+public class AstarParking {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("\n--= PROGRAMA DE BÚSQUEDA HEURÍSTICA CON A* =--\n");
+
+	}
+
+}
