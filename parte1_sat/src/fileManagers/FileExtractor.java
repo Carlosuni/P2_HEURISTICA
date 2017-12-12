d2039d9f4e05c337751ee5094440f317e4706952
hellraider <hellraider01@gmail.com>
2017-12-12 19:16:36 +0100

    master test

--- parte1_sat/src/fileManagers/FileExtractor.java
+++ parte1_sat/src/fileManagers/FileExtractor.java
@@ -1,6 +1,15 @@
 package fileManagers;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import models.Parking;
+
 
 public class FileExtractor {
 	private String nombreArchivo;
@@ -12,14 +21,31 @@ public class FileExtractor {
 	
 	public FileExtractor(String nombreArchivoTXT) {
 		this.nombreArchivo = nombreArchivoTXT;	
-		this.rutaArchivo = "./semillasInput/" + nombreArchivoTXT;
+		this.rutaArchivo = "../semillasInput/" + nombreArchivoTXT;
 	}
 	
 	public Parking extraeParking() {
 		//TODO
 		Parking newParking = new Parking();
 		newParking.setRutaArchivoTXT(rutaArchivo);
-		
+		//leo todas las lineas del archivo y las almaceno en una variable, para luego crear los coches y el parking
+		System.out.println(Paths.get(newParking.getRutaArchivoTXT()).toString());
+		try {
+			FileReader fr = new FileReader(Paths.get(newParking.getRutaArchivoTXT()).toString());
+			BufferedReader bufferedReader = new BufferedReader(fr);
+	        List<String> lines = new ArrayList<String>();
+	        String line = null;
+	        while ((line = bufferedReader.readLine()) != null) {
+	            lines.add(line);
+	        }
+	        bufferedReader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return newParking; 
 	}
